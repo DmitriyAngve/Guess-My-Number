09a6b08a78642044149c46de1ca78503ffc98a26
Dmitriy Angve <kirirch129@gmail.com>
2022-10-23 14:12:27 +0300

    #

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+
+const number = Math.random();
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); // function - event handler, only be called as soon as the event happens ('click')
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
